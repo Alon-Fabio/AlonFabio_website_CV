3c4d2bb931cf7ed54c6119928841daf91e24a395
Alon-Fabio <alon.the.fabio@gmail.com>
2020-05-21 16:50:13 +0300

    Email form update

--- js/contact_me.js
+++ js/contact_me.js
@@ -14,6 +14,12 @@ $(function() {
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
+      const formdata = {
+        name: name,
+          phone: phone,
+          email: email,
+          message: message
+      }
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
@@ -22,12 +28,7 @@ $(function() {
       $.ajax({
         url: "././mail/contact_me.php",
         type: "POST",
-        data: serialize({
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        }),
+        data: formdata,
         cache: false,
         success: function() {
           // Success message
