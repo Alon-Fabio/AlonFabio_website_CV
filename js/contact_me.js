7d5842b39d7cf567577c5defde16edb5831fc3e9
Alon-Fabio <alon.the.fabio@gmail.com>
2020-07-07 13:25:31 +0300

    form redirect

--- js/contact_me.js
+++ js/contact_me.js
@@ -36,11 +36,12 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Thank you " + firstName + "Your message has been sent. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
+          window.location.replace("https://www.alonfabio.com/pages/success")
         },
         error: function() {
           // Fail message
