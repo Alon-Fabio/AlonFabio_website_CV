de183d87b40b7f789088b7ef3a8361c85e007a53
Alon-Fabio <alon.the.fabio@gmail.com>
2021-08-10 19:40:30 +0300

    Set background

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./About.scss";
+import background from "../../styles/img/header-bg.jpg";
 
 function About() {
   return (
-    <div>
+    <div className={"about"} style={{ backgroundImage: `url(${background})` }}>
       <h1>About</h1>
     </div>
   );
