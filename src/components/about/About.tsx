7bbd5be7adf1677e06175f3bb863810230c9a032
Alon-Fabio <alon.the.fabio@gmail.com>
2021-08-14 18:37:04 +0300

    About page: text, fonts, responsive

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./About.scss";
-import background from "../../styles/img/header-bg.jpg";
 
+import AlonHeroPhoto from "../../styles/img/about/mini Alon square.jpg";
 function About() {
+  const [onLoad, setOnLoad] = useState("");
+  useEffect(() => {
+    setOnLoad("onLoad");
+    console.log("changed");
+  }, []);
+
   return (
-    <div className={"about"} style={{ backgroundImage: `url(${background})` }}>
-      <h1>About</h1>
+    <div className={"about"}>
+      <div className={"aboutTextContainer"}>
+        <h1>Welcome</h1>
+        <div className={"AboutDescription text-shadow"}>
+          <p>
+            My name is <span>Alon</span>, and this is my website.
+          </p>
+          <p>
+            I"m a <span>Developer</span>, a <span>Designer</span>, a
+            <span>Photographer</span>, and I"m a <span>Doer</span>.
+          </p>
+          <p>Have a look around</p>
+        </div>
+      </div>
+      <div className={`${onLoad} photoContainer`}>
+        <img src={`${AlonHeroPhoto}`} alt={"to Alon Fabio About page"} />
+      </div>
     </div>
   );
 }
