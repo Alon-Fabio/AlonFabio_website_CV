0b7c1a03515999c1023f51b6138491e5e4ec6dca
Alon-Fabio <alon.the.fabio@gmail.com>
2022-10-29 15:36:34 +0300

    Main page: css, html, imgs

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import "./main.scss";
 
+import image1 from "./images/1.webp";
+import image2 from "./images/2.webp";
+import image3 from "./images/3.webp";
+import image4 from "./images/4.webp";
+
 const Main: React.FC<{ links: Array<string> }> = ({ links }) => {
   console.log(links);
   return (
@@ -10,9 +15,28 @@ const Main: React.FC<{ links: Array<string> }> = ({ links }) => {
           .filter((routeName) => routeName.toString() !== "Main")
           .map((routeName) => (
             <li key={`NavLink-${routeName}`}>
-              <Link to={`/${routeName.toString()}`}>{routeName}</Link>
+              <Link to={`/${routeName.toString()}`}>
+                <h1>{routeName}</h1>
+              </Link>
             </li>
           ))}
+        <li id="Photography">
+          <h1>{"Photography"}</h1>
+          <div id="imageSqCon">
+            <div className="imageSq">
+              <img src={image1} alt=" " />
+            </div>
+            <div className="imageSq">
+              <img src={image2} alt=" " />
+            </div>
+            <div className="imageSq">
+              <img src={image3} alt=" " />
+            </div>
+            <div className="imageSq">
+              <img src={image4} alt=" " />
+            </div>
+          </div>
+        </li>
       </ul>
     </section>
   );
