c760f662ed012502fd912a28dbe38b60a85442ea
Alon-Fabio <alon.the.fabio@gmail.com>
2023-07-08 17:50:35 +0300

    ProPerPage: Ready!

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 // Containers
 import PageHero from "../../containers/PageHero/PageHero";
 // Style
+import "./start.scss";
 // Photos
 import ProfilePic from "../../styles/img/assets/Alon1631w.jpg";
 
@@ -15,29 +16,65 @@ const Start = () => {
   return (
     <div id="Start">
       <PageHero>
-        <div>
-          <div id="start_welcome_texts">
-            <h1>Welcome my site</h1>
-            <h3>{"This is me >>"}</h3>
-            <h2>My name is Alon Fabio, and I'm a pro web designer</h2>
-            <h3>
+        {/* <PageHero images={[{ image: Logo, screenSize: "" }]}> */}
+        <div id="start_hero_container">
+          <h1>
+            <span>Welcome</span> to my site
+          </h1>
+          <div id="start_h_img_con" className="">
+            {/* <h3>{"That's me >>"}</h3> */}
+            <div className="start_img_clip">
+              <img
+                src={ProfilePic}
+                alt="profile_picture"
+                id="start_profile_picture"
+              />
+            </div>
+
+            {/* <h3>
               Here you need to choose if you want to visit my Professional site
               or my Personal site
-            </h3>
-            <h2>Pro web dev and graphic designer</h2>
-            <h2>Per Photography and graphics</h2>
+              </h3>
+              <h2>Pro web dev and graphic designer</h2>
+            <h2>Per Photography and graphics</h2> */}
           </div>
-
-          <img src={ProfilePic} alt="profile_picture" />
+          <h3>
+            I am <span>Alon Fabio</span>
+          </h3>
         </div>
       </PageHero>
-      <section>
-        <ol id="start_ol">
+      <section id="start_proper_sec">
+        {/* <h1>My two sits</h1> */}
+        <ol id="start_ol" className="flexCenter">
           <li key="Professional">
-            <Link to={"/Pro"}>Professional</Link>
+            <h3>
+              <Link to={"/Pro/Services"}>Services</Link>
+            </h3>
+            <Link className="start_li_link" to={"/Pro"}>
+              <h1>
+                <span>Pro</span>
+                <p>fessional</p>
+              </h1>
+            </Link>
           </li>
+          <li id="start_li_dash">/</li>
+          <hr />
           <li key="Personal">
-            <Link to={"/Per"}>Personal</Link>
+            <Link className="start_li_link" to={"/Per"}>
+              <h1>
+                <span>Per</span>
+                <p>sonal</p>
+              </h1>
+            </Link>
+            <h3>
+              <Link to={"/Per/Photography"}>Photography</Link>
+              <br />
+              <Link to={"/Per/Graphics"}>Graphics</Link>
+
+              <br />
+              {/* Articles <sub>soon</sub>
+              <br /> */}
+            </h3>
           </li>
         </ol>
       </section>
