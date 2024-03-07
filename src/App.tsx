544ae4a9e64de313461ad60b6d1adff1cda454bf
Alon-Fabio <alon.the.fabio@gmail.com>
2024-03-07 13:57:28 +0200

    Ready to show

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,21 @@ import Graphics from "./pages/Graphics/Graphics";
 import Start from "./pages/Start/Start";
 import RouterLayout from "./containers/RouterLayout/RouterLayout";
 import LoadAni from "./components/LoadAni/LoadAni";
-// import BasicErrorPage from "./components/Errors/BasicErrorPage";
-// import ErrorBoundary from "./components/Errors/ErrorBoundary";
 
 const Fadminbio = lazy(() => import("./pages/Fadminbio/Fadminbio"));
 const Services = lazy(() => import("./pages/Services/Services"));
 function App() {
   return (
     <div className="App">
-      {/* <ErrorBoundary errorPage={<BasicErrorPage />}> */}
       <Suspense fallback={<LoadAni />}>
         <Routes>
           <Route path="*" index element={<Start />} />
-
           <Route path="/Pro" element={<RouterLayout />}>
             {/* Add an About Pro page */}
             <Route index path="*" element={<Services />} />
             <Route path="Services" element={<Services />} />
             <Route path="Contact" element={<Contact />} />
           </Route>
-
           <Route path="/Per" element={<RouterLayout />}>
             {/* Add an About Pro page */}
             <Route index path="*" element={<Photography />} />
@@ -38,11 +33,9 @@ function App() {
             <Route path="Photography" element={<Photography />} />
             <Route path="Graphics" element={<Graphics />} />
           </Route>
-
           <Route path="Fadminbio" element={<Fadminbio />} />
         </Routes>
       </Suspense>
-      {/* </ErrorBoundary> */}
     </div>
   );
 }
